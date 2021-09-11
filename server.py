a79c503d484991a474ae20e3a54e5868630a5bf9
gabrielnsd <gabrielnsdantas@gmail.com>
2021-09-11 09:52:00 -0300

    setup flask

--- server.py
+++ server.py
@@ -1,1 +1,16 @@
-print("Hello World")
+from flask import Flask, request, json
+import mysql.connector
+from mysql.connector import Error
+from mysql.connector import errorcode
+from os import environ
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "Hello world"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
