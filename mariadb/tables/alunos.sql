7e938e08d7e3927b48c4f930594ed49026af8d78
sabrinaaquino <sabrina.aquino@hotmail.com>
2021-09-14 18:35:02 -0300

    adding insert.sql

--- mariadb/tables/alunos.sql
+++ mariadb/tables/alunos.sql
@@ -5,8 +5,8 @@ CREATE TABLE IF NOT EXISTS ALUNOS(
     n_cartao VARCHAR(16),
     link_call VARCHAR(30),
     objetivo VARCHAR(300) NOT NULL,
-    cpf_aluno INT,
-    cpf_instrutor INT,
+    cpf_aluno VARCHAR(11),
+    cpf_instrutor VARCHAR(11),
     FOREIGN KEY (cpf_aluno) REFERENCES USUARIOS(cpf),
     FOREIGN KEY (cpf_instrutor) REFERENCES INSTRUTORES(cpf_instrutor),
     PRIMARY KEY(cpf_aluno)
