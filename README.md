d1ffa73c2fa15db01be763b491677bfdb96501f7
gabrielnsd <gabrielnsdantas@gmail.com>
2021-09-15 18:55:23 -0300

    criacao de rotas de insercao

--- README.md
+++ README.md
@@ -12,3 +12,76 @@ Em seguida execute o comando:
 `docker-compose up`
 
 A url de acesso à API será mostrada no terminal assim que os conteiners estiverem sendo executados.
+
+## Documentacão API:
+
+Como utilizar as rotas REST:
+
+### /add_usuário
+
+Método: POST  
+json:
+
+```json
+{
+  "cpf": "12345678910",
+  "nome": "nome",
+  "dt_inicio": "2021-09-14",
+  "email": "email@email.com",
+  "senha": "senha",
+  "adm": false
+}
+```
+
+### /add_aluno
+
+Método: POST  
+json:
+
+```json
+{
+  "cpf": "12345678910",
+  "nome": "nome",
+  "dt_inicio": "2021-09-14",
+  "email": "email@email.com",
+  "senha": "senha",
+  "adm": false,
+  "altura": 1.7,
+  "peso": 70,
+  "n_cartao": "1234123412341234",
+  "cpf_instrutor": "01987654321"
+}
+```
+
+### /add_instrutor
+
+Método: POST  
+json:
+
+```json
+{
+  "cpf": "12345678910",
+  "nome": "nome",
+  "dt_inicio": "2021-09-14",
+  "email": "email@email.com",
+  "senha": "senha",
+  "adm": false,
+  "salario": 3000,
+  "especializacao": "pilates",
+  "conta": "ag. 123 cc. 456"
+}
+```
+
+### /add_frequencia
+
+Método: POST  
+json:
+
+```json
+{
+  "hora_entrada": "07:40",
+  "hora_saida": "18:40",
+  "data": "2021/09/15",
+  "u_cpf": "12345678910"
+}
+```
---
+++ helper_functions/queries_functions.py
@@ -0,0 +1,39 @@
+import mysql.connector
+from mysql.connector import Error
+from mysql.connector import errorcode
+
+
+DB_HOST = 'mariadb'  # environ.get('DB_HOST')
+DB_NAME = 'academia'  # environ.get('DB_NAME')
+DB_USER = 'acadadmin'  # environ.get('DB_USER')
+DB_PASSWORD = 'acad123'  # environ.get('MARIADB_PASSWORD')
+
+
+def run_insert_query(query, values, table_name):
+    connection = get_database_connection()
+    res = ''
+    id = None
+    try:
+        cursor = connection.cursor()
+        cursor.execute(query, values)
+        connection.commit()
+        id = cursor.lastrowid
+        if id is not None:
+            res += 'Record with id('+str(id) + \
+                ') inserted successfully into '+table_name+' table'
+        else:
+            res += str(cursor.rowcount) + \
+                ' Record inserted successfully into '+table_name+' table'
+        print(res)
+        cursor.close()
+    except mysql.connector.Error as error:
+        res += "Failed to inser record into table {}".format(error)
+        print(res)
+    finally:
+        if connection.is_connected():
+            connection.close()
+    return (res, id)
+
+
+def get_database_connection():
+    return mysql.connector.connect(host=DB_HOST, database=DB_NAME, user=DB_USER, password=DB_PASSWORD)
